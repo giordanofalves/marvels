8ca30fb27a30805a8ef41b173f7d607991c47dd4
Giordano Alves <giordanofalves@gmail.com>
2016-10-18 11:24:08 -0300

    (close #31) Remove duplicated code.
    Edit README;

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,17 +6,4 @@ module ApplicationHelper
   def separator(content)
     content_tag(:spam, content, class: "separator")
   end
-
-  def alphabetic_pagination
-    if user_signed_in?
-      content_tag(:div, class: "btn-toolbar") do
-        content_tag(:div, class: "btn-group btn-group-sm") do
-          ("A".."Z").map do |letter|
-            concat(link_to(letter, update_view_path(letter),
-                           class: "btn btn-default"))
-          end
-        end
-      end
-    end
-  end
 end
