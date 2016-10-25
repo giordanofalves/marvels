66cb82ad3a144b156be769a39e8f36f1815db398
Giordano Alves <giordanofalves@gmail.com>
2016-10-25 14:51:23 -0300

    (close #51) Create character table.
    * Migration;
    * Factory
    * Model Spec;

---
+++ spec/models/character_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe Character, type: :model do
+  describe "Should Respond" do
+    it { should respond_to(:character_id) }
+    it { should respond_to(:name) }
+    it { should respond_to(:description) }
+    it { should respond_to(:url) }
+    it { should respond_to(:modified_at) }
+    it { should respond_to(:created_at) }
+    it { should respond_to(:updated_at) }
+  end
+
+  describe "Validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:modified_at) }
+  end
+
+  describe "Factory" do
+    let(:character) { build(:character) }
+
+    it { expect(character).to be_a Character }
+    it { expect(character).to be_valid }
+  end
+end
